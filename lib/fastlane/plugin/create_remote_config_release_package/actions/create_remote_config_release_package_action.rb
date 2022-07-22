5a313b4918228f345d686ccfd69766159e3843b2
George Nyakundi <dev.things.ke@gmail.com>
2022-07-22 17:31:04 +0200

    fixing more rubocop issues

--- lib/fastlane/plugin/create_remote_config_release_package/actions/create_remote_config_release_package_action.rb
+++ lib/fastlane/plugin/create_remote_config_release_package/actions/create_remote_config_release_package_action.rb
@@ -85,13 +85,11 @@ module Fastlane
         Dir.chdir("..") do
           sh("zip -FSr provisioning_package.zip manifest.json #{zip_name}; rm #{zip_name}; rm manifest.json; rm project.json")
         end
-        
         UI.success("provisioning_package.zip creation Completed")
 
         FileUtils.mv('../provisioning_package.zip', './releases/provisioning_package.zip')
 
         UI.success("provisioning_package.zip has been moved to releases folder successfully")
-
       end
 
       def self.return_value
